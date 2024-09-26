777b626196469505d30c6fda9a019bfa64615379
Benjamín Isasmendi C <isasmendi223@gmail.com>
2024-09-26 20:39:32 -0300

    Initial files

---
+++ src/com/intellingcamera/Main.java
@@ -0,0 +1,32 @@
+package com.intellingcamera;
+
+import com.intellingcamera.camera.Camera;
+import org.opencv.core.Core;
+
+import java.awt.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+
+                Camera camera = new Camera();
+                camera.setVisible(true);
+
+                new Thread(new Runnable() {
+
+                    public void run() {
+
+                        camera.startCamera();
+
+                    }
+                }).start();
+            }
+        });
+
+    }
+}
---
+++ src/com/intellingcamera/camera/Camera.java
@@ -0,0 +1,81 @@
+package com.intellingcamera.camera;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Camera extends JFrame {
+
+    private JLabel cameraScreen;
+    private JButton captureButton;
+    private VideoCapture videoCapture;
+    private Mat image;
+    private boolean isClicked = false;
+
+    public Camera() {
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        setLayout(null);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Intento de camara inteligente :v");
+        setSize(630, 580);
+
+        cameraScreen = new JLabel();
+        cameraScreen.setBounds(0, 0, 640, 480);
+        add(cameraScreen);
+
+        captureButton = new JButton("capturar");
+        captureButton.setBounds(300, 480, 80, 40);
+        add(captureButton);
+
+        captureButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                isClicked = true;
+            }
+        });
+
+    }
+
+    public void startCamera() {
+
+        videoCapture = new VideoCapture(0); // Asegúrate de que la cámara se inicialice correctamente
+        image = new Mat();
+        byte[] imageData;
+
+        ImageIcon icon;
+        while (true) {
+            if (videoCapture.read(image)) {
+
+                final MatOfByte buffer = new MatOfByte();
+                Imgcodecs.imencode(".jpg", image, buffer);
+
+                imageData = buffer.toArray();
+                icon = new ImageIcon(imageData);
+                cameraScreen.setIcon(icon);
+
+                if (isClicked) {
+                    String nameFile = JOptionPane.showInputDialog("ingresa el nombre de la imagen.");
+
+                    if (nameFile == null || nameFile.trim().isEmpty()) {
+                        nameFile = "poto";
+                    }
+
+                    Imgcodecs.imwrite("imagenes/" + nameFile + ".jpg", image);
+
+                    isClicked = false;
+                }
+            } else {
+                System.out.println("No se pudo capturar la imagen");
+            }
+        }
+    }
+}
